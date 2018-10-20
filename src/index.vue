b5de129c41474239626f1a31d5351204e9a75f22
Kananek T <info.dvgamer@gmail.com>
2018-10-20 17:22:24 +0700

    fixe icon

--- src/index.vue
+++ src/index.vue
@@ -23,15 +23,9 @@
         @input="handleInput"
         @change="handleChange">
       <span class="mx-input-append">
-        <slot name="calendar-icon">
-          <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 200 200" class="mx-calendar-icon">
-            <rect x="13" y="29" rx="14" ry="14" width="174" height="158" fill="transparent" />
-            <line x1="46" x2="46" y1="8" y2="50" />
-            <line x1="154" x2="154" y1="8" y2="50" />
-            <line x1="13" x2="187" y1="70" y2="70" />
-            <text x="50%" y="135" font-size="90" stroke-width="1" text-anchor="middle" dominant-baseline="middle">{{new Date().getDate()}}</text>
-          </svg>
-        </slot>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16px" height="16px">
+          <path d="M 6 1 L 6 3 L 5 3 C 3.9 3 3 3.9 3 5 L 3 19 C 3 20.1 3.9 21 5 21 L 19 21 C 20.1 21 21 20.1 21 19 L 21 5 C 21 3.9 20.1 3 19 3 L 18 3 L 18 1 L 16 1 L 16 3 L 8 3 L 8 1 L 6 1 z M 5 5 L 6 5 L 8 5 L 16 5 L 18 5 L 19 5 L 19 7 L 5 7 L 5 5 z M 5 9 L 19 9 L 19 19 L 5 19 L 5 9 z"/>
+        </svg>
       </span>
       <span
         v-if="showClearIcon"
